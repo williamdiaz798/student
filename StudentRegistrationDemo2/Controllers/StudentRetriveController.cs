8f740ca843161d47be2a25f4d85fff6e9b761d8e
williamdiaz798 <49824200+williamdiaz798@users.noreply.github.com>
2021-03-17 14:35:34 -0600

    creacion de proyecto

---
+++ StudentRegistrationDemo2/Controllers/StudentRetriveController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using StudentRegistrationDemo2.Models;
+
+namespace StudentRegistrationDemo2.Controllers
+{
+    public class StudentRetriveController : ApiController
+    {
+        public List<Student> GetAllStudents()
+        {
+            return StudentRegistration.getInstance().getAllStudent();
+        }
+    }
+}
